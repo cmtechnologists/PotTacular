430bf713ae147131cd82101197979c8bfce7a5be
jonnygarrity <jonnygarrity@gmail.com>
2019-08-02 14:59:19 -0500

    Added MSTest test project.

---
+++ server/pottacular-api/pottacular-api.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace pottacular_api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

b0e5f875436e96ed93410b4bfd361fa17a792dd9
jonnygarrity <jonnygarrity@gmail.com>
2019-11-12 20:39:15 -0600

    Added information to teardown script.

--- server/pottacular-api/pottacular-api/Scripts/teardown.ps1
+++ server/pottacular-api/pottacular-api/Scripts/teardown.ps1
@@ -1,3 +1,6 @@
+echo "stopping and removing api project containers..."
 docker rm -f PottacularApi
 docker rm -f pottacular-api_mongo-express_1
 docker rm -f pottacular-api_mongo_1
+echo "list of remaining running containers..."
+docker ps
